72ce425c52bc1ee6996bc9b542a375552c82f9f2
angelsofy23 <sofiabilyk23@gmail.com>
2024-01-21 19:42:06 +0000

    Crop nas images e mudança de tamanhos
    .

--- Logar.js
+++ Logar.js
@@ -9,11 +9,14 @@ function Login() {
     let user = document.getElementById("caixatextouser");
     let pass = document.getElementById("caixatextopass");
 
+    //Para os dados das caixas de texto coincidirem com os dados do dicionário
     if (dados[user.value] === pass.value) {
 
         window.location.href = "jogo.html";
         return true;
     }
+
+    //Verificar se as caixas de texto estão vazias
 
     if(user.value == "" || pass.value == "")
     {
