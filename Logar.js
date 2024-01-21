95e40a2a3bb90a817bc9bb98560dce4b2c98f6b0
Iago Paulo <tessimago@gmail.com>
2024-01-21 16:11:22 +0000

    Fixed stuff de alinhamento

--- Logar.js
+++ Logar.js
@@ -1,17 +1,21 @@
-let user = document.getElementById("caixatextouser");
-let pass = document.getElementById("caixatextopass");
-let dados = [
 
+let dados = [
     {"username": "angelsofy23", "password": "Morganaisthebestchamp"}
 ]
 
+
 function Login()
 {
+    let user = document.getElementById("caixatextouser");
+    let pass = document.getElementById("caixatextopass");
     for(i = 0; i < dados.length; i++)
     {
-        if(dados[i].username && dados[i].password === user.value && pass.value)
+        if(dados[i].username === user.value && dados[i].password === pass.value)
         {
-            window.location.href = "jogo.html";
+            document.location.href = "jogo.html";
+            return true;
         }
     }
+
+    return false;
 }
