9cc6f2f9130f14f203d29df7d85c70479cf403a0
Iago Paulo <tessimago@gmail.com>
2024-01-22 20:05:19 +0000

    Geracao dos clientes mais bonitinha
    Apesar de ainda ter uns bugs caso os clientes desaparecam, coisa que ainda nao acontece entao ta tudo bem hehe

--- GeradorClientes.js
+++ GeradorClientes.js
@@ -1,17 +1,39 @@
+/* Array de dicionarios que guarda os clientes que podem aparecer */
 const clients = [
     {url : 'Images/Clientes/tile001.png', name : 'tile001'},
     {url : 'Images/Clientes/tile002.png', name : 'tile002'},
     {url : 'Images/Clientes/tile003.png', name : 'tile003'},
     {url : 'Images/Clientes/tile004.png', name : 'tile004'},
-]
+];
 
+/* Pega o div que contem os clientes */
+const div = document.getElementById('clientes');
+
+/* Array que guarda os clientes que ja foram gerados */
+let clientesInGame = [];
+
+/* Funcao de gerar um cliente */
 function generate(){
+    if(clientesInGame.length >= 5) return; // Nao cria mais que 5 clientes)
+    
+    /* Escolhe um cliente aleatoriamente */
     const random = clients[Math.floor(Math.random() * clients.length)];
+
+    /* Cria o elemento de imagem e adiciona a pagina */
     const imgElement = document.createElement('img');
     imgElement.src = random.url;
     imgElement.alt = random.name;
-    imgElement.style.zIndex = '3';
-    document.body.appendChild(imgElement);
+    imgElement.style.zIndex = '3'; // Para nao ficar atras do cenario
+    imgElement.id = "cliente"; // Para ser aplicado os efeitos do css com este id
+
+    // Posiciona o cliente na tela dependendo de quantos clientes ja existem
+    imgElement.style.left = (700 + 100*clientesInGame.length) + 'px'; 
+
+    // Adiciona o cliente ao array de clientes gerados e a pagina
+    div.appendChild(imgElement);
+    clientesInGame.push(imgElement);
 }
-generate();generate();generate();
+
+setInterval(generate, 1000); // Cria um cliente a cada 5 segundos
+
 
