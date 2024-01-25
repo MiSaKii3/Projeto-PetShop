fc342a8d34e7c27330b66dedf902a4f72669490b
Camila Costa <camilacostix@gmail.com>
2024-01-25 19:06:33 +0000

    Telas win e game over
    Fiz as duas e fiz um código para que elas apareçam ao fim de seis minutos (game over se os ganhos forem menores que o e win se forem maiores), só que o contador deixou de andar

--- ContadorTempo.js
+++ ContadorTempo.js
@@ -6,16 +6,30 @@ function formatarTempo(segundos) {
     return `${String(horas).padStart(2, '0')}:${String(minutos).padStart(2, '0')}:${String(segundosFormatados).padStart(2, '0')}`;
 }
 
+// Função para exibir a tela de game over
+function exibirGameOver() {
+    document.getElementById('game-over-tela').classList.remove('hidden');
+}
+// Função para exibir a tela de win
+function exibirYouWin() {
+    document.getElementById('win-tela').classList.remove('hidden2');
+}
+
 // Função para atualizar o contador de tempo
 function atualizarContador() {
-    //save
-    //...
-    const totalmilisegudos = 600000;
+    const totalSegundos = 360; // 6 minutos
+    dinheiroTotal = document.getElementById('dinheiroTotal')
 
-    if (segundos < totalmilisegudos) {
-        segundos++;
-        document.getElementById('contador').textContent = formatarTempo(segundos);
+    if (segundos >= totalSegundos) {
+        if (dinheiroTotal < 0) {
+            exibirGameOver("Game Over");
+        } else if (dinheiroTotal > 0) {
+            exibirYouWin("You Win");
+        }
+        clearInterval(intervalo);
     }
+    
 }
+
 let segundos = 0;
 const intervalo = setInterval(atualizarContador, 1000); // Atualiza a cada segundo
