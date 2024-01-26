09794aa6cbc142e85d3f9682b808a29b45b6dd6d
angelsofy23 <sofiabilyk23@gmail.com>
2024-01-26 11:38:09 +0000

    .
    .

--- ContadorTempo.js
+++ ContadorTempo.js
@@ -31,13 +31,13 @@ function atualizarContador() {
     if (segundos == 360) {
         
         if (dinheiroTotal.textContent <= 300) {
-            let ganhos = document.getElementById("ganhos-dia-lose");
+            let ganhos = document.getElementById("ganhos-dia");
             ganhos.textContent = dinheiroTotal.textContent;
             exibirGameOver();
             clearInterval(intervalo);
 
         } else if (dinheiroTotal.textContent > 300) {
-            let ganhos = document.getElementById("ganhos-dia-win");
+            let ganhos = document.getElementById("ganhos-dia");
             ganhos.textContent = dinheiroTotal.textContent;
             exibirYouWin();
             clearInterval(intervalo);
